996c95c3e84107e7bf8346ec09c1267322751dce
mhdshabeer <mohammedshabeer2945@gmail.com>
2025-01-22 20:30:45 +0530

    data ingestion complete

---
+++ src/CNNclassifier/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,32 @@
+from CNNclassifier.config.configurations import ConfigurationManager
+from CNNclassifier.constants import __init
+from CNNclassifier.components.data_ingestion import DataIngestion
+from CNNclassifier.__init import logger
+
+cfp = __init.CONFIG_FILE_PATH
+pfp = __init.PARAMS_FILE_PATH
+
+STAGE_NAME = "Data Ingestion stage"
+
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager(cfp, pfp)
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
